5f529f131af8f6254a39c9aca82dc035e3e3ff80
Benjamin Friedrich <friedrich.benjamin@gmail.com>
2018-10-02 17:49:14 +0200

    SOL-69 implemented file record modification + dto fix

--- src/main/kotlin/com/xama/client/files/Files.kt
+++ src/main/kotlin/com/xama/client/files/Files.kt
@@ -24,8 +24,10 @@ import java.util.concurrent.CompletableFuture
  */
 class Files private constructor(){
     data class UserDto(val id: UUID, val name: String?, val firstName: String?, val lastName: String?, val fullName: String?)
-    data class FileDto(val id: UUID, val folderId: UUID?, val size: Int, val createdDateUtc: String, val updatedDateUtc: String, val user: UserDto?)
+    data class FileDto(val id: UUID, val folderId: UUID?, val name: String, val mimeType: String, val size: Int, val createdDateUtc: String, val updatedDateUtc: String, val user: UserDto?)
     data class GetFilesResponseDto(val totalCount: Int, val page: Int, val perPage: Int, val items: List<FileDto>)
+
+    private data class FileChangeDto(val name: String?, val folderId: UUID?)
 
     class Client (val http: Http, val config: Config){
         companion object {
@@ -78,8 +80,7 @@ class Files private constructor(){
         }
 
 
-        fun postFile(fileName: String, fileUrl: URL): CompletableFuture<FileDto> {
-
+        fun uploadFile(fileName: String, fileUrl: URL): CompletableFuture<FileDto> {
             val multiValueMap = LinkedMultiValueMap<String, Any>()
             multiValueMap[fileName] = ExtendedResource(fileName, fileUrl.readBytes())
 
@@ -101,6 +102,33 @@ class Files private constructor(){
                     )
                     .thenApply(capture);
         }
+
+
+        fun changeFile(fileId: UUID, newFileName: String? = null, newFolderId: UUID? = null): CompletableFuture<FileDto> {
+            check(newFileName != null || newFolderId != null) {
+                "cannot file [fileId=$fileId]: either new file name or folder id must be specified"
+            }
+
+            val requestUrl = fileUri(fileId)
+            val capture = Capture.empty<FileDto>()
+
+            return http.put(requestUrl)
+                    .contentType(MediaType.APPLICATION_JSON_UTF8)
+                    .accept(MediaType.APPLICATION_JSON_UTF8)
+                    .header(HttpHeaders.USER_AGENT, config.userAgent)
+                    .headers(oauthHeaders(
+                            config = config,
+                            httpMethod = HttpMethod.PUT,
+                            requestPath = requestUrl
+                    ))
+                    .body(FileChangeDto(name = newFileName, folderId = newFolderId))
+                    .dispatch(Navigators.series(),
+                            Bindings.on(HttpStatus.Series.SUCCESSFUL).call(FileDto::class.java, capture),
+                            Bindings.anySeries().call(ProblemRoute.problemHandling(Route.call { p -> handleProblem(p) }))
+                    )
+                    .thenApply(capture);
+        }
+
 
         fun deleteFile(fileId: UUID): CompletableFuture<Void> {
             val requestUrl = fileUri(fileId)
