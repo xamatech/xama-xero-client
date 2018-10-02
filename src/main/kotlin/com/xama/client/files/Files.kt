81e97c6c4ada44924ff92ae0c1d213857918f03d
Benjamin Friedrich <friedrich.benjamin@gmail.com>
2018-10-02 19:15:39 +0200

    SOL-69 added supported for most query parameters of the files retrieval endpoint

--- src/main/kotlin/com/xama/client/files/Files.kt
+++ src/main/kotlin/com/xama/client/files/Files.kt
@@ -1,5 +1,6 @@
 package com.xama.client.files
 
+import com.google.common.collect.HashMultimap
 import com.xama.client.Config
 import com.xama.client.ExtendedResource
 import com.xama.client.handleProblem
@@ -27,29 +28,61 @@ class Files private constructor(){
     data class FileDto(val id: UUID, val folderId: UUID?, val name: String, val mimeType: String, val size: Int, val createdDateUtc: String, val updatedDateUtc: String, val user: UserDto?)
     data class GetFilesResponseDto(val totalCount: Int, val page: Int, val perPage: Int, val items: List<FileDto>)
 
+    enum class SortDirection {
+        ASC,
+        DESC
+    }
+
+    enum class SortField(val attributeName: String) {
+        CREATED("CreatedDateUTC"),
+        NAME("Name"),
+        SIZE("Size")
+    }
+
     private data class FileChangeDto(val name: String?, val folderId: UUID?)
+
+
+    // Bindings.on(HttpStatus.Series.SUCCESSFUL).call({ response: ClientHttpResponse, reader: MessageReader -> println(response.body.bufferedReader().use { it.readText() }) }),
+
 
     class Client (val http: Http, val config: Config){
         companion object {
             const val BASE_URL = "https://api.xero.com/files.xro/1.0/files"
+            const val DEFAULT_PAGE_SIZE = 100
+            const val DEFAULT_PAGE = 1
+            val DEFAULT_SORT = Pair(SortField.CREATED, SortDirection.DESC)
         }
 
         private fun fileUri(fileId: UUID) = "$BASE_URL/$fileId"
 
-        //Bindings.on(HttpStatus.Series.SUCCESSFUL).call({ response: ClientHttpResponse, reader: MessageReader -> println(response.body.bufferedReader().use { it.readText() }) }),
+        fun getFiles(
+                pageSize: Int = DEFAULT_PAGE_SIZE,
+                page: Int = DEFAULT_PAGE,
+                sortParameter: Pair<SortField, SortDirection> = DEFAULT_SORT
 
-        fun getFiles(): CompletableFuture<GetFilesResponseDto> {
+        ): CompletableFuture<GetFilesResponseDto> {
+
+            val queryParameterPairs = listOf(
+                    Pair("pagesize", pageSize.toString()),
+                    Pair("page", page.toString())
+                    // Pair("sort", "${sortParameter.first.attributeName} ${sortParameter.second.name}") FIXME problem with signature, if specified
+            )
+
+            val queryParams = HashMultimap.create<String, String>()
+            queryParameterPairs.forEach { queryParams.put(it.first, it.second) }
+
+
             val capture = Capture.empty<GetFilesResponseDto>()
             return http.get(BASE_URL)
                     .contentType(MediaType.APPLICATION_JSON_UTF8)
                     .accept(MediaType.APPLICATION_JSON_UTF8)
-                    // .ifModifiedSince() TODO
+                    .queryParams(queryParams)
                     .header(HttpHeaders.USER_AGENT, config.userAgent)
                     .headers(oauthHeaders(
                             config = config,
                             httpMethod = HttpMethod.GET,
-                            requestPath = BASE_URL
-                            // TODO query params
+                            requestPath = BASE_URL,
+                            parameters = queryParameterPairs
                     ))
                     .dispatch(Navigators.series(),
                             Bindings.on(HttpStatus.Series.SUCCESSFUL).call(GetFilesResponseDto::class.java, capture),
@@ -88,7 +121,6 @@ class Files private constructor(){
             return http.post(BASE_URL)
                     .contentType(MediaType.MULTIPART_FORM_DATA)
                     .accept(MediaType.APPLICATION_JSON_UTF8)
-                    // .ifModifiedSince() TODO
                     .header(HttpHeaders.USER_AGENT, config.userAgent)
                     .headers(oauthHeaders(
                             config = config,
